5375d2a925572395c8dfedcc82717138302142f1
Eugene <etesting007@gmail.com>
2022-07-27 14:33:31 -0400

    test pad tokens

--- bittensor/_neuron/text/core_server/nucleus_impl.py
+++ bittensor/_neuron/text/core_server/nucleus_impl.py
@@ -78,10 +78,10 @@ class server(torch.nn.Module):
 
         # Define PAD Token = EOS Token (GPT2 generate convention, when PAD Token is None)
         # https://github.com/huggingface/transformers/blob/49c8c67fb815a277405f84dea4a66353e19fb347/tests/models/gpt2/test_modeling_gpt2.py#L532
-        if self.pre_model.config.pad_token_id is None and self.pre_model.config.eos_token_id is not None:
-            self.pre_model.config.pad_token_id = self.pre_model.config.eos_token_id
-        elif self.pre_model.config.pad_token_id is None and self.pre_model.config.bos_token_id is not None:
-            self.pre_model.config.pad_token_id = self.pre_model.config.bos_token_id
+        if self.tokenizer.pad_token_id is None and self.tokenizer.eos_token_id is not None:
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        elif self.tokenizer.pad_token_id is None and self.tokenizer.bos_token_id is not None:
+            self.tokenizer.pad_token_id = self.tokenizer.bos_token_id
 
         self.tokenizer = prep_tokenizer(self.tokenizer)
         self.to_translation_map = get_translation_map(self.tokenizer, self.std_tokenizer)
